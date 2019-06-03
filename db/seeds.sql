7d62088c5d83d3d3d7dc9efbaafe5fa7f3fc29b2
kdublam <kdublam@gmail.com>
2019-06-03 12:27:03 -0700

    redoing repo with gitignore

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shack Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jumbo Jack', true);
+
---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burgers = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(burger_name, cb) {
+    orm.insertOne("burgers", burger_name, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(burger_id, cb) {
+    orm.updateOne("burgers", burger_id, function(res) {
+      cb(res);
+    });
+  }
+  // delete: function(condition, cb) {
+  //   orm.delete("burgers", condition, function(res) {
+  //     cb(res);
+  //   });
+  // }
+};
+
+// Export the database functions for the controller (burgers_Controller.js).
+module.exports = burgers;
